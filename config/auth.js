7ad422774c4cd46fa0f79d382682cb5932e5095f
Jfilippo06 <jfilippo04@gmail.com>
2022-11-03 20:59:43 +0100

    .env.example

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,4 @@ require('dotenv').config()
 
 module.exports = {
     rounds: process.env.AUTH_ROUNDS,
-    admin: process.env.AUTH_PASSWORD_ADMIN,
-    standard: process.env.AUTH_PASSWORD_STANDARD,
 }
